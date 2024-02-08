91bcead72340216622fabf7168310f0ebf940964
riicardojs <ricardo.jeronymo@beaverbit.com.br>
2024-02-08 14:15:26 -0300

    feat: add redis and rank for polls

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -43,6 +44,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         });
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
       }
     }
 
@@ -64,6 +67,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return reply.status(201).send();
   });
